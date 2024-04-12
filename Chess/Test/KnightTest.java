b799af66e2b663b8869bbd58bc171ba8296f1798
Joviiiiii <jovinscki@digitalready.org>
2024-04-12 16:26:02 -0400

    Knight Tests

--- Chess/Test/KnightTest.java
+++ Chess/Test/KnightTest.java
@@ -1,5 +1,50 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 public class KnightTest {
+    @Test
+    public void canItMoveTrueVertical() {
+        new Board();
+
+        Knight knight = new Knight("KB",ChessColor.Black, "♞");
+;
+
+        Assertions.assertTrue(knight.canItMove(5,5, 6, 7));
+    }
+
+    @Test
+    public void canItMoveFalseEndXMoreThanOneAway(){
+        new Board();
+
+        Knight knight = new Knight("KB",ChessColor.Black, "♞");
+
+        Assertions.assertFalse(knight.canItMove(5,5, 7, 7));
+    }
+    @Test
+    public void canItMoveTrueWithEnemyPieceAtSpot() {
+        new Board();
+
+
+        Pawn pawn = new Pawn("pawn1", ChessColor.White, "♟");
+
+        Knight knight = new Knight("KB",ChessColor.Black, "♞");
+
+        Board.applyMove(new Tile(pawn), 0, 1, 6, 7);
+
+
+        Assertions.assertTrue(knight.canItMove(5,5, 6, 7));
+
+    }
+
+    @Test
+    public void canItMoveHorizontal() {
+        new Board();
+
+
+        Knight knight = new Knight("KB",ChessColor.Black, "♞");
+
+
+        Assertions.assertTrue(knight.canItMove(5,5, 3, 4));
+
+    }
     
 }
